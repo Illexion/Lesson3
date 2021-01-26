45c54fd1022820f4a5f17d85e61f66987f627085
Illexion <digrayman@yandex.ru>
2021-01-26 17:39:40 +0300

    Добил сравнения строк.

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -11,37 +11,42 @@ public class Array {
         String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli",
                 "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom",
                 "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-
         String guessWord = randomWord(words);
 
         Arrays.fill(unknownArray, "#");
 
-        for (String s : unknownArray) {
+        for (String s : unknownArray) {// посоветовала идея) такую конструкцию применять предпочтительнее обчного if ?
             System.out.print(s);
         }
 
-
-        System.out.println(words.length);
-        System.out.println(words[3].length());
-        System.out.println(words[3].charAt(0));
-        System.out.println("введи слово");
+        System.out.println(" Введи слово:");
         String userAnswer = sc.nextLine();
-        sc.close();
-        for (int i = 0; i < Math.min(guessWord.length(),userAnswer.length() ) ; i++) {
-            if(userAnswer.charAt(i) == guessWord.charAt(i)){
-                System.out.println("Верно");
-                unknownArray[i] = String.valueOf(userAnswer.charAt(i));
+
+
+        while (!guessWord.equals(userAnswer)) {
+            for (int i = 0; i < Math.min(guessWord.length(), userAnswer.length()); i++) { // Цикл по длинне минимальной строки
+                if (userAnswer.charAt(i) == guessWord.charAt(i)) {
+                    unknownArray[i] = String.valueOf(userAnswer.charAt(i));
+                }
             }
+
+            for (String s : unknownArray) {
+                System.out.print(s);
+            }
+
+            System.out.println(" Введи еще слово:");
+            userAnswer = sc.nextLine();
+
         }
-        for (String s : unknownArray) {
-            System.out.print(s);
-        }
+        sc.close();
+        System.out.println("Угадал! Это и правда - " + guessWord);
     }
-    public static String randomWord(String[] wordArray){
+
+    public static String randomWord(String[] wordArray) {
         Random random = new Random();
         int randomWordNumber = random.nextInt(wordArray.length + 1);
         String guessWord = wordArray[randomWordNumber];
-        System.out.println(guessWord);
+        System.out.println("Программа загадала: " + guessWord);
         return guessWord;
     }
 }
